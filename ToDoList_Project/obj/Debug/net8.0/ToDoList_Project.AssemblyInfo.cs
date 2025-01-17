477a98e4837ae1de3dd883a55c3c3cdc9ea03c70
Steven Serrao <stevenserraowork@gmail.com>
2025-01-17 09:52:25 +0200

    componentized the todolist as well as the individual todo object

--- ToDoList_Project/obj/Debug/net8.0/ToDoList_Project.AssemblyInfo.cs
+++ ToDoList_Project/obj/Debug/net8.0/ToDoList_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beff920cbe9006b55acd6f6b4938ce82d8ff3185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd6ffcf2a60a7ee95952021460f199ec6d4bade2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
